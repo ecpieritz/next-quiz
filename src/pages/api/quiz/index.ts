14f2df3f75e62637a81dd3f48cc94d52f8d86fbb
ecpieritz <ecpieritz@gmail.com>
2024-03-17 18:54:26 -0300

    feat: adds function that shuffles questions

--- src/pages/api/quiz/index.ts
+++ src/pages/api/quiz/index.ts
@@ -1,6 +1,8 @@
+import { shuffle } from "../../../../functions/arrays"
 import questions from "../questionsDatabase"
 
 export default function handler(req, res) {
+  const ids = questions.map(question => question.id)
   
-  res.status(200).json(questions.map(question => question.id))
+  res.status(200).json(shuffle(ids))
 }

55c9f8a177aedafe70344fb39512ec2b6953c4f6
ecpieritz <ecpieritz@gmail.com>
2024-03-19 10:38:32 -0300

    feat: add back div

--- components/Answer.tsx
+++ components/Answer.tsx
@@ -15,7 +15,7 @@ export default function Answer(props: AnswerProps) {
   return (
     <div className={styles.answer} onClick={() => props.onResponse(props.index)}>
       <div className={styles.contentAnswer}>
-        <div className={styles.front}>
+        {/* <div className={styles.front}>
           <div className={styles.letter}
             style={{backgroundColor: props.letterBgCollor}}
           >
@@ -24,10 +24,20 @@ export default function Answer(props: AnswerProps) {
           <div className={styles.value}>
             <p className={styles.optionP}>{answer.value}</p>
           </div>
-        </div>
+        </div> */}
 
         <div className={styles.back}>
-          
+          {answer.correct ? (
+            <div className={styles.correct}>
+              <div><h4>A resposta certa é...</h4></div>
+              <div className={styles.value}>{answer.value}</div>
+            </div>
+          ) : (
+            <div className={styles.incorrect}>
+              <div><h4>A resposta informada está errada!</h4></div>
+              <div className={styles.value}>{answer.value}</div>
+            </div>
+          )}
         </div>
         
       </div>
